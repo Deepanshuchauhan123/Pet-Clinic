e9c3aae75d6811771874bb3cf2cff1debbcdcc68
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-05-30 11:44:14 +0530

    Interfaces for Pet Clinic Services. Close #5

---
+++ pet-clinic-data/src/main/java/com/deepanshu/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.deepanshu.petclinic.services;
+
+import com.deepanshu.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/deepanshu/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.deepanshu.petclinic.services;
+
+import com.deepanshu.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
